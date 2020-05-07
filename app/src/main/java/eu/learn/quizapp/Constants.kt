592dfa1201c34706b05d08c39fcb72d562e12e24
Stachu82 <stachelek.l@gmail.com>
2020-05-07 23:34:38 +0200

    Quizz App - add questions

--- app/src/main/java/eu/learn/quizapp/Constants.kt
+++ app/src/main/java/eu/learn/quizapp/Constants.kt
@@ -8,7 +8,7 @@ object Constants {
         val questionList = ArrayList<Question>()
 
         val que1 = Question(1,
-        "Jakiego kraju to jest flaga",
+        "Jakiego kraju to jest flaga?",
         R.drawable.ic_flag_of_argentina,
         "Argentyna","Austria", "Australia", "Brazylia",
             1)
@@ -17,7 +17,7 @@ object Constants {
 
         // 2
         val que2 = Question(
-            2, "What country does this flag belong to?",
+            2, "Jakiego kraju to jest flaga?",
             R.drawable.ic_flag_of_australia,
             "Angola", "Austria",
             "Australia", "Armenia", 3
@@ -27,7 +27,7 @@ object Constants {
 
         // 3
         val que3 = Question(
-            3, "What country does this flag belong to?",
+            3, "Jakiego kraju to jest flaga?",
             R.drawable.ic_flag_of_brazil,
             "Belarus", "Belize",
             "Brunei", "Brazil", 4
@@ -37,7 +37,7 @@ object Constants {
 
         // 4
         val que4 = Question(
-            4, "What country does this flag belong to?",
+            4, "Jakiego kraju to jest flaga?",
             R.drawable.ic_flag_of_belgium,
             "Bahamas", "Belgium",
             "Barbados", "Belize", 2
@@ -47,7 +47,7 @@ object Constants {
 
         // 5
         val que5 = Question(
-            5, "What country does this flag belong to?",
+            5, "Jakiego kraju to jest flaga?",
             R.drawable.ic_flag_of_fiji,
             "Gabon", "France",
             "Fiji", "Finland", 3
@@ -57,7 +57,7 @@ object Constants {
 
         // 6
         val que6 = Question(
-            6, "What country does this flag belong to?",
+            6, "Jakiego kraju to jest flaga?",
             R.drawable.ic_flag_of_germany,
             "Germany", "Georgia",
             "Greece", "none of these", 1
@@ -67,7 +67,7 @@ object Constants {
 
         // 7
         val que7 = Question(
-            7, "What country does this flag belong to?",
+            7, "Jakiego kraju to jest flaga?",
             R.drawable.ic_flag_of_denmark,
             "Dominica", "Egypt",
             "Denmark", "Ethiopia", 3
@@ -77,7 +77,7 @@ object Constants {
 
         // 8
         val que8 = Question(
-            8, "What country does this flag belong to?",
+            8, "Jakiego kraju to jest flaga?",
             R.drawable.ic_flag_of_india,
             "Ireland", "Iran",
             "Hungary", "India", 4
@@ -87,7 +87,7 @@ object Constants {
 
         // 9
         val que9 = Question(
-            9, "What country does this flag belong to?",
+            9, "Jakiego kraju to jest flaga?",
             R.drawable.ic_flag_of_new_zealand,
             "Australia", "New Zealand",
             "Tuvalu", "United States of America", 2
@@ -97,7 +97,7 @@ object Constants {
 
         // 10
         val que10 = Question(
-            10, "What country does this flag belong to?",
+            10, "Jakiego kraju to jest flaga?",
             R.drawable.ic_flag_of_kuwait,
             "Kuwait", "Jordan",
             "Sudan", "Palestine", 1
--- app/src/main/java/eu/learn/quizapp/QuizQuestionActivity.kt
+++ app/src/main/java/eu/learn/quizapp/QuizQuestionActivity.kt
@@ -1,12 +1,83 @@
 package eu.learn.quizapp
 
+import android.graphics.Color
+import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import androidx.core.content.ContextCompat
+import kotlinx.android.synthetic.main.activity_quiz_question.*
 
-class QuizQuestionActivity : AppCompatActivity() {
+class QuizQuestionActivity : AppCompatActivity(), View.OnClickListener {
+
+
+    private var mCurrentPosition: Int = 1
+    private var mQuestionList: ArrayList<Question>? = null
+    private var mCurrentSelected: Int = 0
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quiz_question)
+
+        mQuestionList = Constants.getQuestion()
+        setQuestion()
+
+        tv_option_one.setOnClickListener(this)
+        tv_option_two.setOnClickListener(this)
+        tv_option_tree.setOnClickListener(this)
+        tv_option_four.setOnClickListener(this)
+
+    }
+
+    private fun setQuestion(){
+        val question = mQuestionList!![mCurrentPosition - 1]
+        progress_bar.progress = mCurrentPosition
+        tv_progress_bar.text = "$mCurrentPosition / ${progress_bar.max}"
+        tv_question.text = question!!.question
+        iv_image.setImageResource(question.image)
+        tv_option_one.text = question.optionOne
+        tv_option_two.text = question.optionTwo
+        tv_option_tree.text = question.optionTree
+        tv_option_four.text = question.optionFour
+
+        setDefaultView()
+    }
+
+    private fun setDefaultView(){
+        val options = ArrayList<TextView>()
+        options.add(0,tv_option_one)
+        options.add(1, tv_option_two)
+        options.add(2, tv_option_tree)
+        options.add(3, tv_option_four)
+
+        for (option in options){
+            option.setTextColor(Color.parseColor("@colors/textColorSecondary"))
+            option.typeface = Typeface.DEFAULT
+            option.background = ContextCompat.getDrawable(this,
+                R.drawable.default_option_border_bg)
+
+        }
+    }
+
+    private fun setSelectedOption(tv: TextView, selectedOption: Int){
+        setDefaultView()
+        mCurrentSelected = selectedOption
+        tv.setTextColor(Color.parseColor("@colors/textColorPrimary"))
+        tv.setTypeface(tv.typeface, Typeface.BOLD)
+        tv.background = ContextCompat.getDrawable(this,
+            R.drawable.selected_option_border_bg)
+
+    }
+    override fun onClick(v: View?) {
+        when(v?.id){
+            R.id.tv_option_one -> {setSelectedOption(tv_option_one, 1)}
+            R.id.tv_option_two -> {setSelectedOption(tv_option_two, 2)}
+            R.id.tv_option_tree -> {setSelectedOption(tv_option_tree, 3)}
+            R.id.tv_option_four -> {setSelectedOption(tv_option_four, 4)}
+            
+
+        }
     }
 }
